7dc66a2c2b648d615145639bafd84e47be101161
vitalya-dev <vitalya.dev@gmail.com>
2019-03-02 07:43:21 +0300

    Drag and Drop

---
+++ Run for prize/Assets/Controllers/DragController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragController : MonoBehaviour {
+	private RaycastHit2D hit;
+
+	void Update() {
+		// Make sure the user pressed the mouse down
+		if (!Input.GetMouseButtonDown(0)) {
+			return;
+		}
+
+		// We need to actually hit an object
+		hit = Physics2D.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+			Camera.main.ScreenPointToRay(Input.mousePosition).direction);
+		if (!hit.collider)
+			return;
+
+		StartCoroutine("Drag");
+	}
+
+	private IEnumerator Drag() {
+		while (Input.GetMouseButton(0)) {
+			Vector2 offset = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition) - hit.point;
+			hit.transform.Translate(offset);
+			hit.point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			yield return null;
+		}
+		Vector3 rounded_position = new Vector3(Mathf.RoundToInt(hit.transform.position.x),
+			Mathf.RoundToInt(hit.transform.position.y),
+			Mathf.RoundToInt(hit.transform.position.z)
+		);
+		hit.transform.position = rounded_position;
+	}
+}
---
+++ Run for prize/Assets/Controllers/MotionController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionController : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	public bool collisionLeft;
+	public bool collisionRight;
+	public bool collisionAbove;
+	public bool collisionBelow;
+
+	// Update is called once per frame
+	void Update() {
+		Debug.DrawRay(transform.position, Vector2.down / 2, Color.red);
+		Debug.DrawRay(transform.position, Vector2.left / 2, Color.red);
+		Debug.DrawRay(transform.position, Vector2.right / 2, Color.red);
+		Debug.DrawRay(transform.position, Vector2.up / 2, Color.red);
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, collisionMask);
+		if (hit.collider)
+			collisionBelow = true;
+		else
+			collisionBelow = false;
+
+		hit = Physics2D.Raycast(transform.position, Vector2.left, 0.6f, collisionMask);
+		if (hit.collider)
+			collisionLeft = true;
+		else
+			collisionLeft = false;
+
+		hit = Physics2D.Raycast(transform.position, Vector2.right, 0.6f, collisionMask);
+		if (hit.collider)
+			collisionRight = true;
+		else
+			collisionRight = false;
+
+		hit = Physics2D.Raycast(transform.position, Vector2.up, 0.6f, collisionMask);
+		if (hit.collider)
+			collisionAbove = true;
+		else
+			collisionAbove = false;
+	}
+}
