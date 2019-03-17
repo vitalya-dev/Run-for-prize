dd57e5c75fc696f322b36cc2f6d5df71b0d98688
vitalya-dev <vitalya.dev@gmail.com>
2019-03-17 17:34:36 +0300

    Some sort of menu

--- Run for prize/Assets/Controllers/MenuController.cs
+++ Run for prize/Assets/Controllers/MenuController.cs
@@ -5,17 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+	public bool isActive;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+		if (isActive)
+			gameObject.transform.position = new Vector3(0, 0, -5);
+		else
+			gameObject.transform.position = new Vector3(0, 0, 5);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape) && !isActive) {
+			gameObject.transform.Translate(new Vector3(0, 0, -10));
+			isActive = true;
+		} else if (Input.GetKeyDown(KeyCode.Escape) && isActive) {
+			gameObject.transform.Translate(new Vector3(0, 0, 10));
+			isActive = false;
+		}
 	}
 
 	public void NewGame() {
-		SceneManager.LoadScene(0);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 }
