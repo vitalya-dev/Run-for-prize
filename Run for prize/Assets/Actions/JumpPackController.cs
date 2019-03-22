a7a1a2384be8aacad2607386f7090fb366b128a5
vitalya-dev <vitalya.dev@gmail.com>
2019-03-22 08:05:43 +0300

    Background Highlight

---
+++ Run for prize/Assets/Actions/JumpPackController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPackController : Action {
+	public Vector2 direction;
+
+	public override void StartDrag() {
+		foreach (var s in GameObject.FindGameObjectsWithTag("Background")) {
+			if (s.GetComponent<HighlightController>() != null)
+				s.GetComponent<HighlightController>().highlight = true;
+		}
+	}
+
+	public override void StopDrag() {
+		foreach (var s in GameObject.FindGameObjectsWithTag("Background")) {
+			if (s.GetComponent<HighlightController>() != null)
+				s.GetComponent<HighlightController>().highlight = false;
+		}
+	}
+}
--- Run for prize/Assets/Controllers/JumpPackController.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class JumpPackController : MonoBehaviour {
-	public Vector2 direction;
-}
