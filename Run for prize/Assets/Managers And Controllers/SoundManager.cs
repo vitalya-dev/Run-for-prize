b91845cfd4030413feb2cdc884b813f03e8075a0
vitalya-dev <vitalya.dev@gmail.com>
2019-03-27 08:49:31 +0300

    Add music

---
+++ Run for prize/Assets/Managers And Controllers/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioClip[] soundtrack;
+
+	// Use this for initialization
+	void Start() {
+		if (soundtrack.Length > 0) {
+			int i = Random.Range(0, soundtrack.Length);
+			PlayClipAt(soundtrack[i], transform.position);
+		}
+
+	}
+
+	public AudioSource PlayClipAt(AudioClip clip, Vector3 position, float volume = 1f) {
+		if (clip != null) {
+			GameObject o = new GameObject("SoundFX" + clip.name);
+			o.transform.position = position;
+			Destroy(o, clip.length);
+
+			AudioSource music = o.AddComponent<AudioSource>();
+			music.clip = clip;
+
+			music.volume = volume;
+			music.Play();
+
+			return music;
+		} else
+			return null;
+	}
+}
