d76510b0c6146aa37a82bd122f50662225193be5
vitalya-dev <vitalya.dev@gmail.com>
2019-04-08 08:04:04 +0300

    Start WOrking On Palette

--- Run for prize/Assets/Editor/MenuItems.cs
+++ Run for prize/Assets/Editor/MenuItems.cs
@@ -7,9 +7,14 @@ public class MenuItems {
 	[MenuItem("Run For Prize/Add Level")]
 	public static void LevelAdd() {
 		GameObject level_meta = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Level.prefab", typeof(GameObject)) as GameObject;
-		
+
 		GameObject level = PrefabUtility.InstantiatePrefab(level_meta) as GameObject;
 		level.transform.position = Vector3.zero;
 		Selection.activeGameObject = level;
+	}
+
+	[MenuItem("Run For Prize/Show Palette")]
+	public static void PalleteShow() {
+		PaletteWindow.ShowPalette();
 	}
 }
---
+++ Run for prize/Assets/Editor/PaletteWindow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class PaletteWindow : EditorWindow {
+	public static PaletteWindow instance;
+
+	public static void ShowPalette() {
+		instance = (PaletteWindow) EditorWindow.GetWindow(typeof(PaletteWindow));
+	}
+}
