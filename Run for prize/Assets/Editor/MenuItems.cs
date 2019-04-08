80b635b4bc38bb4b69c3e56be41e2196e132a6ed
vitalya-dev <vitalya.dev@gmail.com>
2019-04-08 07:43:23 +0300

    Level Create

--- Run for prize/Assets/Editor/MenuItems.cs
+++ Run for prize/Assets/Editor/MenuItems.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 
 public class MenuItems {
 
 	[MenuItem("Run For Prize/Add Level")]
 	public static void LevelAdd() {
-		Debug.Log("Add Level");
+		GameObject level_meta = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Level.prefab", typeof(GameObject)) as GameObject;
+		
+		GameObject level = PrefabUtility.InstantiatePrefab(level_meta) as GameObject;
+		level.transform.position = Vector3.zero;
+		Selection.activeGameObject = level;
 	}
 }
