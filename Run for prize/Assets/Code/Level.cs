ace230acc0e7b63a6df48eb093d772cdecb15d4f
vitalya-dev <vitalya.dev@gmail.com>
2019-04-09 02:43:45 +0300

    Gizmo at right space

--- Run for prize/Assets/Code/Level.cs
+++ Run for prize/Assets/Code/Level.cs
@@ -6,19 +6,23 @@ public class Level : MonoBehaviour {
 	public int rows;
 
 	private void OnDrawGizmos() {
+		DrawGizmos(new Vector3(-1.0f/2.0f * rows, -1.0f/2.0f * cols, 0));
+	}
+
+	private void DrawGizmos(Vector3 origin) {
 		Matrix4x4 old_matrix = Gizmos.matrix;
 		Gizmos.matrix = transform.localToWorldMatrix;
 
-		Gizmos.DrawLine(new Vector3(0, 0, 0), new Vector3(cols, 0, 0));
-		Gizmos.DrawLine(new Vector3(0, rows, 0), new Vector3(cols, rows, 0));
-		Gizmos.DrawLine(new Vector3(0, 0, 0), new Vector3(0, rows, 0));
-		Gizmos.DrawLine(new Vector3(cols, 0, 0), new Vector3(cols, rows, 0));
+		Gizmos.DrawLine(origin, origin + new Vector3(cols, 0, 0));
+		Gizmos.DrawLine(origin + new Vector3(0, rows, 0), origin + new Vector3(cols, rows, 0));
+		Gizmos.DrawLine(origin, origin + new Vector3(0, rows, 0));
+		Gizmos.DrawLine(origin + new Vector3(cols, 0, 0), origin + new Vector3(cols, rows, 0));
 
 		for (int i = 1; i < cols; i++) {
-			Gizmos.DrawLine(new Vector3(i, 0, 0), new Vector3(i, rows, 0));
+			Gizmos.DrawLine(origin + new Vector3(i, 0, 0), origin + new Vector3(i, rows, 0));
 		}
 		for (int j = 1; j < rows; j++) {
-			Gizmos.DrawLine(new Vector3(0, j, 0), new Vector3(cols, j, 0));
+			Gizmos.DrawLine(origin + new Vector3(0, j, 0), origin + new Vector3(cols, j, 0));
 		}
 
 		Gizmos.matrix = old_matrix;
