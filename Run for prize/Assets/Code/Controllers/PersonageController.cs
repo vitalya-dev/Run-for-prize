0fc70761cbb33fb8aa0421325d443d6b1ed71190
vitalya-dev <vitalya.dev@gmail.com>
2019-04-11 22:34:29 +0300

    States

---
+++ Run for prize/Assets/Code/Controllers/PersonageController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PersonageController : MonoBehaviour {
+
+	[HideInInspector]
+	public bool rolling = false;
+
+	public float velocity;
+
+	public Vector2 face {
+		get {
+			return transform.up;
+		}
+		set {
+			transform.rotation = Quaternion.LookRotation(transform.forward, value);
+		}
+	}
+
+	public void Roll(float axis, float forsage = 1) {
+		if (!rolling)
+			StartCoroutine(RollRoutine(
+				transform.position + new Vector3(axis, 0, 0),
+				Quaternion.LookRotation(transform.forward, axis * transform.right),
+				forsage
+			));
+	}
+
+	private IEnumerator RollRoutine(Vector3 finalPosition, Quaternion finalRotation, float forsage) {
+		rolling = true;
+
+		Vector3 positionDeparture = transform.position;
+		Quaternion rotationDeparture = transform.rotation;
+
+		float time_needed = Vector3.Distance(positionDeparture, finalPosition) / (velocity * forsage);
+		float elapsed_time = 0;
+
+		
+		while (Vector3.Distance(transform.position, finalPosition) > 0.001f) {
+			transform.position = Vector3.Lerp(positionDeparture, finalPosition, elapsed_time / time_needed);
+			transform.rotation = Quaternion.Slerp(rotationDeparture, finalRotation, elapsed_time / time_needed);
+			elapsed_time += Time.deltaTime;
+			yield return null;
+		}
+
+		transform.position = finalPosition;
+		transform.rotation = finalRotation;
+
+		rolling = false;
+	}
+}
---
+++ Run for prize/Assets/Code/State Machines/HBaseFSM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HBaseFSM : StateMachineBehaviour {
+	protected PersonageController personage;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		personage = animator.transform.GetComponent<PersonageController>();
+	}
+}
