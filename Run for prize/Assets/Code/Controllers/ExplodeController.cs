10edb99e1c6fb82e4b0d5ccc69e917f151a253b7
vitalya-dev <vitalya.dev@gmail.com>
2019-04-15 22:39:50 +0300

    Explode

--- Run for prize/Assets/Code/Controllers/ExplodeController.cs
+++ Run for prize/Assets/Code/Controllers/ExplodeController.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ExplodeController : MonoBehaviour {
 
+	public ParticleSystem explosion_prefab;
+
 	public void Explode() {
-		Debug.Log("Explode");
+		ParticleSystem explosion = Instantiate(explosion_prefab);
+		explosion.Play();
+
+		GameObject.Destroy(explosion.gameObject, explosion.startLifetime);
 		GameObject.Destroy(gameObject);
 	}
 }
--- Run for prize/Assets/Code/State Machines/HeroFlyState.cs
+++ Run for prize/Assets/Code/State Machines/HeroFlyState.cs
@@ -11,7 +11,7 @@ public class HeroFlyState : HBaseFSM {
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		base.OnStateUpdate(animator, stateInfo, layerIndex);
-
+	
 		if (!personage.moving && personage.collisionAhead)
 			animator.SetTrigger("Crash");
 		else
