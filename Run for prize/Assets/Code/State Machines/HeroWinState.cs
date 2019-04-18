4c01c8865f2dadd59f161718f21915a0f61d06be
vitalya-dev <vitalya.dev@gmail.com>
2019-04-18 07:12:30 +0300

    Okey we now have win state. Time to work on directories

--- Run for prize/Assets/Code/State Machines/HeroWinState.cs
+++ Run for prize/Assets/Code/State Machines/HeroWinState.cs
@@ -1,12 +1,20 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class HeroWinState : StateMachineBehaviour {
+public class HeroWinState : HBaseFSM {
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		base.OnStateEnter(animator, stateInfo, layerIndex);
+
+		/* ================================================================= */
+		personage.GetComponent<ExplodeController>().Explode();
+		personage.collisionAhead.GetComponent<ExplodeController>().Explode();
+		/* ================================================================= */
+
+		GameObject.Find("GameManager").GetComponent<GameManager>().LevelRestart();
+		
+	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
