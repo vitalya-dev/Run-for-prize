ed3470d4939da64911b758f3c29750daecf4dc49
Vitalya <vitalya.dev@gmail.com>
2019-04-23 08:19:28 +0300

    Drag And Drop

--- Run for prize/Assets/Code/Level.cs
+++ Run for prize/Assets/Code/Level.cs
@@ -1,35 +1,28 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-
-public class Level : MonoBehaviour
-{
+public class Level : MonoBehaviour {
     public int cols;
     public int rows;
 
-
-    private void OnDrawGizmos()
-    {
-        DrawGizmos(new Vector3(-0.5f * cols, -0.5f * rows, 0));
+    private void OnDrawGizmos () {
+        DrawGizmos (new Vector3 (-0.5f * cols, -0.5f * rows, 0));
     }
 
-    private void DrawGizmos(Vector3 origin)
-    {
+    private void DrawGizmos (Vector3 origin) {
         Matrix4x4 old_matrix = Gizmos.matrix;
         Gizmos.matrix = transform.localToWorldMatrix;
 
-        Gizmos.DrawLine(origin, origin + new Vector3(cols, 0, 0));
-        Gizmos.DrawLine(origin + new Vector3(0, rows, 0), origin + new Vector3(cols, rows, 0));
-        Gizmos.DrawLine(origin, origin + new Vector3(0, rows, 0));
-        Gizmos.DrawLine(origin + new Vector3(cols, 0, 0), origin + new Vector3(cols, rows, 0));
+        Gizmos.DrawLine (origin, origin + new Vector3 (cols, 0, 0));
+        Gizmos.DrawLine (origin + new Vector3 (0, rows, 0), origin + new Vector3 (cols, rows, 0));
+        Gizmos.DrawLine (origin, origin + new Vector3 (0, rows, 0));
+        Gizmos.DrawLine (origin + new Vector3 (cols, 0, 0), origin + new Vector3 (cols, rows, 0));
 
-        for (int i = 1; i < cols; i++)
-        {
-            Gizmos.DrawLine(origin + new Vector3(i, 0, 0), origin + new Vector3(i, rows, 0));
+        for (int i = 1; i < cols; i++) {
+            Gizmos.DrawLine (origin + new Vector3 (i, 0, 0), origin + new Vector3 (i, rows, 0));
         }
-        for (int j = 1; j < rows; j++)
-        {
-            Gizmos.DrawLine(origin + new Vector3(0, j, 0), origin + new Vector3(cols, j, 0));
+        for (int j = 1; j < rows; j++) {
+            Gizmos.DrawLine (origin + new Vector3 (0, j, 0), origin + new Vector3 (cols, j, 0));
         }
 
         Gizmos.matrix = old_matrix;
---
+++ Run for prize/Assets/Code/Managers/DragManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DragManager : MonoBehaviour {
+
+	void Update() {
+		// Make sure the user pressed the mouse down
+		if (!Input.GetMouseButtonDown(0)) {
+			return;
+		}
+
+		// We need to actually hit an object
+		RaycastHit2D hit = Physics2D.Raycast(
+			Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+			Vector2.zero,
+			Mathf.Infinity
+		);
+		if (!hit.collider || hit.collider.GetComponent<ActionController>() == null)
+			return;
+
+		StartCoroutine(Drag(hit));
+	}
+
+	private IEnumerator Drag(RaycastHit2D hit) {
+		ActionController action = hit.collider.GetComponent<ActionController>();
+
+		Vector3 old_pos = hit.transform.position;
+		hit.transform.position = new Vector3(old_pos.x, old_pos.y, Camera.main.transform.position.z + 1);
+
+		while (Input.GetMouseButton(0)) {
+			Vector2 offset = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - hit.point;
+			hit.transform.Translate(offset);
+			hit.point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			yield return null;
+		}
+
+		Vector3 rounded_position = new Vector3(Mathf.RoundToInt(hit.transform.position.x),
+			Mathf.RoundToInt(hit.transform.position.y),
+			Mathf.RoundToInt(old_pos.z)
+		);
+		hit.transform.position = rounded_position;
+
+		yield return null;
+	}
+}
