363cefac66954f070df4dc3082a80354a715249d
Vitalya <vitalya.dev@gmail.com>
2019-04-28 00:37:45 +0300

    Check for dirty spaces.

--- Run for prize/Assets/Code/Managers/DragManager.cs
+++ Run for prize/Assets/Code/Managers/DragManager.cs
@@ -22,23 +22,42 @@ public class DragManager : MonoBehaviour {
 	}
 
 	private IEnumerator Drag(RaycastHit2D hit) {
-		Action action = hit.collider.GetComponent<Action>();
+		hit.collider.enabled = false;
 
+		/* =========================================================== */
+		Action action = hit.collider.GetComponent<Action>();
+		/* =========================================================== */
+
+		/* =========================================================== */
 		Vector3 old_pos = hit.transform.position;
 		hit.transform.position = new Vector3(old_pos.x, old_pos.y, Camera.main.transform.position.z + 1);
+		/* =========================================================== */
 
+		/* =========================================================== */
 		while (Input.GetMouseButton(0)) {
 			Vector2 offset = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - hit.point;
 			hit.transform.Translate(offset);
 			hit.point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			yield return null;
 		}
+		/* =========================================================== */
 
+		/* =========================================================== */
 		Vector3 rounded_position = new Vector3(Mathf.RoundToInt(hit.transform.position.x),
 			Mathf.RoundToInt(hit.transform.position.y),
 			Mathf.RoundToInt(old_pos.z)
 		);
-		hit.transform.position = rounded_position;
+		if (!Physics2D.Raycast(
+				Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+				Vector2.zero,
+				Mathf.Infinity
+			))
+			hit.transform.position = rounded_position;
+		else
+			hit.transform.position = old_pos;
+		/* =========================================================== */
+
+		hit.collider.enabled = true;
 
 		yield return null;
 	}
