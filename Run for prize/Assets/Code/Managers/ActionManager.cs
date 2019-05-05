847180659ceb36f10b6b075a8036b2d1184a1c08
Vitalya <vitalya.dev@gmail.com>
2019-05-05 20:00:03 +0300

    New way to move actions without drag and drop.

---
+++ Run for prize/Assets/Code/Managers/ActionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour {
+	public Action[] actions;
+
+	private Queue<Action> action_stack;
+	private Dictionary<Action, Vector3> actions_pos;
+
+	void Start() {
+		action_stack = new Queue<Action>(actions);
+		/* ===================================================================== */
+		actions_pos = new Dictionary<Action, Vector3>();
+		foreach (var action in actions) actions_pos[action] = action.transform.position;
+		/* ===================================================================== */
+	}
+
+	public Action pop() {
+		if (action_stack.Count > 0)
+			return action_stack.Dequeue();
+		else return null;
+	}
+
+	public void push(Action action) {
+		action_stack.Enqueue(action);
+		action.transform.position = actions_pos[action];
+
+	}
+}
--- Run for prize/Assets/Code/Managers/InputManager.cs
+++ Run for prize/Assets/Code/Managers/InputManager.cs
@@ -9,5 +9,26 @@ public class InputManager : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			FindObjectOfType<GameManager>().Pause();
 		}
+		if (Input.GetMouseButtonDown(0)) {
+			Action action = GameObject.FindObjectOfType<ActionManager>().pop();
+			if (action) {
+				Vector3 pos = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+				pos = new Vector3(
+					Mathf.Floor(pos.x),
+					Mathf.Ceil(pos.y),
+					Mathf.Round(action.transform.position.z)
+				);
+				action.transform.position = pos;
+			}
+		}
+		if (Input.GetMouseButtonDown(1)) {
+			RaycastHit2D hit = Physics2D.Raycast(
+				Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+				Vector2.zero,
+				Mathf.Infinity
+			);
+			if (hit.collider && hit.collider.GetComponent<Action>())
+				GameObject.FindObjectOfType<ActionManager>().push(hit.collider.GetComponent<Action>());
+		}
 	}
 }
