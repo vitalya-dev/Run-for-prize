9f0f2de0438b19d1d53936bdb418ada0613fc0ef
Vitalya <vitalya.dev@gmail.com>
2019-05-18 07:59:57 +0300

    Arrow is relative.

--- Run for prize/Assets/Code/Controllers/Actions/ArrowAction.cs
+++ Run for prize/Assets/Code/Controllers/Actions/ArrowAction.cs
@@ -1,7 +1,27 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ArrowAction : Action {
-	public Vector2 from_direction;
-	public Vector2 to_direction;
+	[SerializeField]
+	private Vector2 _from_direction;
+	[SerializeField]
+	private Vector2 _to_direction;
+
+	public Vector2 from_direction {
+		get {
+			return new Vector2(_from_direction.x * transform.parent.right.x, _from_direction.y * transform.parent.up.y);
+		}
+		set {
+			_from_direction = value;
+		}
+	}
+
+	public Vector2 to_direction {
+		get {
+			return new Vector2(_to_direction.x * transform.parent.right.x, _to_direction.y * transform.parent.up.y);
+		}
+		set {
+			_to_direction = value;
+		}
+	}
 }
