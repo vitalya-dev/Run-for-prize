3dd9f8a53cb9701ac68916f084c036a6a1cf4efe
Vitalya <vitalya.dev@gmail.com>
2019-05-23 08:04:50 +0300

    Highlight cell.

--- Run for prize/Assets/Code/Managers/InputManager.cs
+++ Run for prize/Assets/Code/Managers/InputManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 	private bool _times_out = false;
-
 	public bool times_out {
 		get {
 			return _times_out;
@@ -12,6 +11,8 @@ public class InputManager : MonoBehaviour {
 			_times_out = value;
 		}
 	}
+
+	public GameObject highlight_cell;
 
 	void Update() {
 		/* ################################################################## */
@@ -51,6 +52,15 @@ public class InputManager : MonoBehaviour {
 			if (hit.collider && hit.collider.GetComponent<Action>())
 				GameObject.FindObjectOfType<ActionController>().push(hit.collider.GetComponent<Action>());
 		}
+		/* ################################################################## */
+		highlight_cell.SetActive(true);
+		Vector3 highlight_cell_pos = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+		highlight_cell_pos = new Vector3(
+			Mathf.Floor(highlight_cell_pos.x),
+			Mathf.Ceil(highlight_cell_pos.y),
+			highlight_cell.transform.position.z
+		);
+		highlight_cell.transform.position = highlight_cell_pos;
 		/* ################################################################## */
 	}
 }
