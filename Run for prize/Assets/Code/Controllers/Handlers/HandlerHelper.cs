ca959c80052b4a961bbdec2144466b5220275f02
Vitalya <vitalya.dev@gmail.com>
2019-05-29 16:09:29 +0300

    Work on MouseHandler

---
+++ Run for prize/Assets/Code/Controllers/Handlers/HandlerHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HandlerHelper : MonoBehaviour {
+
+	public void timer_stop_or_level_next() {
+		if (GameObject.FindObjectOfType<GameManager>().current_level.complete)
+			GameObject.FindObjectOfType<GameManager>().LevelNext();
+		else
+			GameObject.FindObjectOfType<Timer>().stop();
+	}
+
+	public void place_action() {
+		RaycastHit2D hit = Physics2D.Raycast(
+			Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+			Vector2.zero,
+			Mathf.Infinity
+		);
+		if (!hit.collider) {
+			Action action = GameObject.FindObjectOfType<ActionController>().pop();
+			if (action) {
+				Vector3 pos = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+				pos = new Vector3(
+					Mathf.Floor(pos.x),
+					Mathf.Ceil(pos.y),
+					Mathf.Round(action.transform.position.z)
+				);
+				action.transform.position = pos;
+			}
+		}
+	}
+
+	public void remove_action() {
+		RaycastHit2D hit = Physics2D.Raycast(
+			Camera.main.ScreenPointToRay(Input.mousePosition).origin,
+			Vector2.zero,
+			Mathf.Infinity
+		);
+		if (hit.collider && hit.collider.GetComponent<Action>())
+			GameObject.FindObjectOfType<ActionController>().push(hit.collider.GetComponent<Action>());
+	}
+
+}
--- Run for prize/Assets/Code/Controllers/Handlers/InputHandler.cs
+++ Run for prize/Assets/Code/Controllers/Handlers/InputHandler.cs
@@ -3,28 +3,11 @@ using UnityEngine;
 using UnityEngine.Events;
 
 
-public class InputHandler : InputHandlerHelper {
-	private bool _disabled = false;
-	public bool disabled {
-		get {
-			return _disabled;
-		}
-		set {
-			_disabled = value;
-		}
-	}
-
-	public  GameObject highlight_cell_prefab;
-	private GameObject highlight_cell;
-
+public class InputHandler : HandlerHelper {
 	public UnityEvent spacekey_callback = new UnityEvent();
 	public UnityEvent rkey_callback = new UnityEvent();
 	public UnityEvent esc_callback = new UnityEvent();
 
-
-	void Start() {
-		 highlight_cell = GameObject.Instantiate(highlight_cell_prefab, Vector3.zero, Quaternion.identity) as GameObject;
-	}
 
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.Space))
@@ -36,53 +19,5 @@ public class InputHandler : InputHandlerHelper {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			esc_callback.Invoke();
 		}
-		/* ################################################################## */
-		if (Input.GetMouseButtonDown(0) && !disabled) {
-			RaycastHit2D hit = Physics2D.Raycast(
-				Camera.main.ScreenPointToRay(Input.mousePosition).origin,
-				Vector2.zero,
-				Mathf.Infinity
-			);
-			if (!hit.collider) {
-				Action action = GameObject.FindObjectOfType<ActionController>().pop();
-				if (action) {
-					Vector3 pos = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
-					pos = new Vector3(
-						Mathf.Floor(pos.x),
-						Mathf.Ceil(pos.y),
-						Mathf.Round(action.transform.position.z)
-					);
-					action.transform.position = pos;
-				}
-			}
-		}
-		/* ################################################################## */
-		if (Input.GetMouseButtonDown(1) && !disabled) {
-			RaycastHit2D hit = Physics2D.Raycast(
-				Camera.main.ScreenPointToRay(Input.mousePosition).origin,
-				Vector2.zero,
-				Mathf.Infinity
-			);
-			if (hit.collider && hit.collider.GetComponent<Action>())
-				GameObject.FindObjectOfType<ActionController>().push(hit.collider.GetComponent<Action>());
-		}
-		/* ################################################################## */
-		highlight_cell.SetActive(true);
-		Vector3 highlight_cell_pos = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
-		highlight_cell_pos = new Vector3(
-			Mathf.Floor(highlight_cell_pos.x),
-			Mathf.Ceil(highlight_cell_pos.y),
-			highlight_cell.transform.position.z
-		);
-		highlight_cell.transform.position = highlight_cell_pos;
-		/* ################################################################## */
-		Action current_action = GameObject.FindObjectOfType<ActionController>().first();
-		if (current_action)
-			highlight_cell.GetComponent<SpriteRenderer>().sprite = current_action.GetComponent<SpriteRenderer>().sprite;
-		else
-			highlight_cell.GetComponent<SpriteRenderer>().sprite = null;
-		if (disabled)
-			highlight_cell.GetComponent<SpriteRenderer>().sprite = null;
-		/* ################################################################## */
 	}
 }
