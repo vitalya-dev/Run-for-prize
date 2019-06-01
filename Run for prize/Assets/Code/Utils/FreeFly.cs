aef586d830ceadfeba30790e58e973871fee9dac
Vitalya <vitalya.dev@gmail.com>
2019-06-01 19:52:30 +0300

    Piggy die scene.

---
+++ Run for prize/Assets/Code/Utils/FreeFly.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreeFly : MonoBehaviour {
+	public Vector2 direction;
+	public float speed;
+	
+	void Start() {
+		direction = direction.normalized;
+	}
+	void Update () {
+		transform.Translate(direction * speed * Time.deltaTime);
+	}
+}
