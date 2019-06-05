6c2957a59fc8d25cc5866d607119c4d27433395f
Vitalya <vitalya.dev@gmail.com>
2019-06-05 16:00:02 +0300

    Okey. Can hide.

---
+++ Run for prize/Assets/Code/Utils/Hide.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hide : MonoBehaviour {
+	public float seconds;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(hide_after(seconds));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator hide_after(float seconds) {
+		yield return new WaitForSeconds(seconds);
+		gameObject.SetActive(false);
+	}
+}
