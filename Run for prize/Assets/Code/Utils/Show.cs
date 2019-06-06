c977d46a9e6b993c8e0d14b9c0ec8ee8f831b80e
Vitalya <vitalya.dev@gmail.com>
2019-06-06 07:01:50 +0300

    Okey. Show.

---
+++ Run for prize/Assets/Code/Utils/Show.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Show : MonoBehaviour {
+	public float seconds;
+
+	// Use this for initialization
+	void Start() {
+		GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0);
+		StartCoroutine(show_after(seconds));
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	private IEnumerator show_after(float seconds) {
+		yield return new WaitForSeconds(seconds);
+		GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+	}
+}
