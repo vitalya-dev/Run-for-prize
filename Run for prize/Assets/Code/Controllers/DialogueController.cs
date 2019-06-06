dc461d5f668f1c12ede043b561d4b5daddc54c12
Vitalya <vitalya.dev@gmail.com>
2019-06-06 17:48:26 +0300

    Dialogue and intro 3.

---
+++ Run for prize/Assets/Code/Controllers/DialogueController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueController : MonoBehaviour {
+    public GameObject[] lines;
+    public float pause_starte;
+    public float pause_between;
+    public float pause_ende;
+
+    public UnityEvent finish_callback;
+
+    void Start() {
+        StartCoroutine(say_it());
+    }
+
+    private IEnumerator say_it() {
+        for (int i = 0; i < lines.Length; i++) {
+            if (i == 0)
+                yield return new WaitForSeconds(pause_starte);
+            else
+                yield return new WaitForSeconds(pause_between);
+            lines[i].SetActive(true);
+        }
+        yield return new WaitForSeconds(pause_ende);
+        finish_callback.Invoke();
+
+    }
+}
