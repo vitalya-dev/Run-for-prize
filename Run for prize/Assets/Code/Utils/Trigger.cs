9229d490342b7286eb8367c83cfdc273cd9474e2
Vitalya <vitalya.dev@gmail.com>
2019-06-08 22:27:48 +0300

    Create triggers.

---
+++ Run for prize/Assets/Code/Utils/Trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour {
+    public UnityEvent trigger_callback;
+
+    void OnEnable() {
+        trigger_callback.Invoke();
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawIcon(transform.position, "Trigger.jpg", true);
+    }
+}
