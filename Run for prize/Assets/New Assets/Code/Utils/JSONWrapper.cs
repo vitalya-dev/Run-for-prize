acf72f26897a415c1a81185d1b796cb48080ef19
Vitalya <vitalya.dev@gmail.com>
2019-06-16 22:37:00 +0300

    Okey. First cutscene intro is over?

---
+++ Run for prize/Assets/New Assets/Code/Utils/JSONWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NewGeneration {
+    public class JSONWrapper : MonoBehaviour {
+        public void setPosition(string position) {
+            SpaceAndTime input = JsonUtility.FromJson<SpaceAndTime>(position);
+            transform.position = new Vector3(input.x, input.y, input.z);
+        }
+    }
+}
