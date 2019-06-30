d2501918e49ce082070ae34577cb2403da680d9d
Vitalya <vitalya.dev@gmail.com>
2019-06-30 12:09:54 +0300

    Okey. Add curtains.

---
+++ Run for prize/Assets/Editor/MenuItems.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+using UnityEditor.SceneManagement;
+
+public class MenuItems {
+
+    [MenuItem("Run For Piggy/Add Level")]
+    public static void AddGlobal() {
+        SceneAsset global = AssetDatabase.LoadAssetAtPath("Assets/New Assets/Scenes/GLOBAL.unity", typeof(SceneAsset))as SceneAsset;
+        EditorSceneManager.OpenScene("Assets/New Assets/Scenes/GLOBAL.unity", OpenSceneMode.Additive);
+    }
+}
