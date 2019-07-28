7e7c6c85be2e1b3cdb7570fc18d10d36f5125658
Vitalya <vitalya.dev@gmail.com>
2019-07-28 15:03:37 +0300

    Dynamic camera.

---
+++ Run for prize/Assets/New Assets/Code/etc/DynamicCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour {
+    private Vector3 init_pos;
+
+    // Start is called before the first frame update
+    void Start() {
+        init_pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        /* ====================================================================== */
+        Vector2 flow = Camera.main.ScreenToViewportPoint(Input.mousePosition) - new Vector3(0.5f, 0.5f);
+        /* ====================================================================== */
+        transform.position = init_pos;
+        transform.Translate(flow.x, flow.y, 0);
+    }
+}
