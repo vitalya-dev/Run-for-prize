06327ab17dc25c7ef4fade0a1764d4fa1ea1813d
Vitalya <vitalya.dev@gmail.com>
2019-08-10 00:31:51 +0300

    Missed connection GAMEPLAY ist complete.

---
+++ Run for prize/Assets/New Assets/Code/Actions/Pipe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NewGeneration.Actions {
+    public class Pipe : Action {
+        public Pipe connection;
+        public Vector2 direction;
+
+        void Start() {
+            Debug.Assert(direction != Vector2.zero);
+        }
+
+        public override void act_on(PiggyController piggy) {
+            if (!connection)
+                GetComponent<Explode>().explode();
+            else if ((direction * -1) == (Vector2)piggy.direction) {
+                piggy.transform.position = connection.transform.position + new Vector3(0.5f, -0.5f);
+                piggy.direction = connection.direction;
+                GetComponent<Explode>().explode();
+            } else {
+                GetComponent<Explode>().explode();
+                piggy.GetComponent<Explode>().explode();
+            }
+        }
+
+        public override void place_on(Collider2D collider) {
+            if (collider)
+                GetComponent<Explode>().explode();
+        }
+    }
+
+}
