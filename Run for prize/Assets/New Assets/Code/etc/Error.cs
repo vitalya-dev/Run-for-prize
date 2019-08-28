dcc7eac844f2dfefbff76096e5b99f2cfe47011e
Vitalya <vitalya.dev@gmail.com>
2019-08-28 08:20:20 +0300

    Error objects is on the scene.

---
+++ Run for prize/Assets/New Assets/Code/etc/Error.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NewGeneration {
+    public class Error : MonoBehaviour {
+        // Start is called before the first frame update
+        void Start() {
+            throw new UnityException("Should't be there: " + SceneManager.GetActiveScene().name);
+        }
+    }
+}
