ff524a8444e7846d33f9a9a6b03bdcbcfab575bf
Vitalya <vitalya.dev@gmail.com>
2019-08-29 08:17:30 +0300

    Level Saver.

---
+++ Run for prize/Assets/New Assets/Code/etc/LevelSaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSaver : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        int current_level = SceneManager.GetActiveScene().buildIndex / 3;
+        if (current_level > PlayerPrefs.GetInt("Current Level", 1))
+            PlayerPrefs.SetInt("Current Level", current_level);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
