12336aeacfac6dafe27399699aa59f5a1e6bf93c
Vitalya <vitalya.dev@gmail.com>
2019-08-29 08:46:18 +0300

    Active and Inactive thumb.

--- Run for prize/Assets/New Assets/Code/etc/LevelLoader.cs
+++ Run for prize/Assets/New Assets/Code/etc/LevelLoader.cs
@@ -7,14 +7,19 @@ using UnityEngine.UI;
 public class LevelLoader : MonoBehaviour {
     public int level_to_load;
 
+    public Sprite active_thumb;
+    public Sprite inactive_thumb;
+
     void Start() {
+        if (level_to_load <= PlayerPrefs.GetInt("Current Level", 1))
+            GetComponent<Image>().sprite = active_thumb;
+        else
+            GetComponent<Image>().sprite = inactive_thumb;
         GetComponent<Button>().onClick.AddListener(load_level);
     }
 
     private void load_level() {
-        if (PlayerPrefs.GetInt("Current Level", 10) >= level_to_load)
+        if (level_to_load <= PlayerPrefs.GetInt("Current Level", 1))
             SceneManager.LoadSceneAsync(level_to_load * 3);
-        else
-            Debug.Log("Ooops its to early");
     }
 }
