c82b1fef7e93124b02ff74b2967a45b1a9482eed
Vitalya <vitalya.dev@gmail.com>
2019-08-30 22:18:35 +0300

    Error fix in first cutscene + debug window

---
+++ Run for prize/Assets/New Assets/Code/Managers/DebugSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugSceneManager : MonoBehaviour {
+    public Text current_level;
+    private string current_level_string_format;
+
+    void Start() {
+        current_level_string_format = current_level.text;
+    }
+
+    void Update() {
+        current_level.text = current_level_string_format.Replace("_$", PlayerPrefs.GetInt("Current Level", 1).ToString());
+    }
+
+    public void reset_current_level() {
+        PlayerPrefs.DeleteKey("Current Level");
+    }
+
+    public void change_current_level(int delta) {
+        int val = PlayerPrefs.GetInt("Current Level", 1) + delta;
+        val = Mathf.Clamp(val, 1, int.MaxValue);
+        /* ------------------------------------------------------------------- */
+        PlayerPrefs.SetInt("Current Level", val);
+    }
+
+}
