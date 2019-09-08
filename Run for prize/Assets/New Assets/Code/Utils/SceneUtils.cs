47772fae399499189631064ea528122da60e50a6
Vitalya <vitalya.dev@gmail.com>
2019-09-08 23:04:13 +0300

    Work on new load system.

---
+++ Run for prize/Assets/New Assets/Code/Utils/SceneUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NewGeneration {
+    public class SceneUtils {
+        public static Scene[] findAllScenes(string pattern) {
+            List<Scene> scenes = new List<Scene>();
+            for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++) {
+                Scene scene = SceneManager.GetSceneByBuildIndex(i);
+                if (scene.name.EndsWith("+"))
+                    scenes.Add(scene);
+            }
+            return scenes.ToArray();
+        }
+    }
+}
--- Run for prize/Assets/New Assets/Code/etc/LevelSaver.cs
+++ Run for prize/Assets/New Assets/Code/etc/LevelSaver.cs
@@ -6,13 +6,8 @@ using UnityEngine.SceneManagement;
 public class LevelSaver : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        int current_level = SceneManager.GetActiveScene().buildIndex / 3;
-        if (current_level > PlayerPrefs.GetInt("Current Level", 1))
+        int current_level = SceneManager.GetActiveScene().buildIndex;
+        if (current_level > PlayerPrefs.GetInt("Current Level", 0))
             PlayerPrefs.SetInt("Current Level", current_level);
-    }
-
-    // Update is called once per frame
-    void Update() {
-
     }
 }
