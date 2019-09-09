9a83bb260f00ea4fb4367ebc831ea8a69c5fe7da
Vitalya <vitalya.dev@gmail.com>
2019-09-09 08:36:11 +0300

    Revert "Work on new load system."
    This reverts commit 47772fae399499189631064ea528122da60e50a6.

--- Run for prize/Assets/New Assets/Code/etc/LevelLoader.cs
+++ Run for prize/Assets/New Assets/Code/etc/LevelLoader.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace NewGeneration {
-    public class LevelLoader : MonoBehaviour {
-        public int level_to_load;
+public class LevelLoader : MonoBehaviour {
+    public int level_to_load;
 
-        public Sprite active_thumb;
-        public Sprite inactive_thumb;
+    public Sprite active_thumb;
+    public Sprite inactive_thumb;
 
-        void Start() {
-            if (level_to_load <= PlayerPrefs.GetInt("Current Level", 1))
-                GetComponent<Image>().sprite = active_thumb;
-            else
-                GetComponent<Image>().sprite = inactive_thumb;
-            GetComponent<Button>().onClick.AddListener(load_level);
-        }
+    void Start() {
+        if (level_to_load <= PlayerPrefs.GetInt("Current Level", 1))
+            GetComponent<Image>().sprite = active_thumb;
+        else
+            GetComponent<Image>().sprite = inactive_thumb;
+        GetComponent<Button>().onClick.AddListener(load_level);
+    }
 
-        private void load_level() {
-            Scene scene_to_load = SceneUtils.findAllScenes("+")[level_to_load];
-            if (scene_to_load.buildIndex <= PlayerPrefs.GetInt("Current Level", 0))
-                SceneManager.LoadSceneAsync(scene_to_load.buildIndex);
-        }
+    private void load_level() {
+        if (level_to_load <= PlayerPrefs.GetInt("Current Level", 1))
+            SceneManager.LoadSceneAsync(level_to_load * 3);
     }
 }
--- Run for prize/Assets/New Assets/Code/etc/LevelSaver.cs
+++ Run for prize/Assets/New Assets/Code/etc/LevelSaver.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class LevelSaver : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        int current_level = SceneManager.GetActiveScene().buildIndex;
-        if (current_level > PlayerPrefs.GetInt("Current Level", 0))
+        int current_level = SceneManager.GetActiveScene().buildIndex / 3;
+        if (current_level > PlayerPrefs.GetInt("Current Level", 1))
             PlayerPrefs.SetInt("Current Level", current_level);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
     }
 }
